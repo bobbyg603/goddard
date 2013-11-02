0d0565231e1d50d45e722576d631bbb7f173e66c
bobbyg603 <bobbyg603@gmail.com>
2013-11-02 18:26:06 +0000

    second commit

---
+++ bin/server.js
@@ -0,0 +1,12 @@
+var util = require('util'),    
+    http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('hello, i know nodejitsu.')
+  res.end();
+}).listen(8000);
+
+/* server started */  
+util.puts('> hello world running on port 8000');
+
