b1fe1c885d169d8854d031a95c55fbc34e8fb36b
bobbyg603 <bobbyg603@gmail.com>
2013-11-10 06:08:30 +0000

    Added requestHandlers, listeners and postData support

--- bin/server.js
+++ bin/server.js
@@ -7,20 +7,24 @@ function start(route, handle) {
 
 	function onRequest (request, response) {
 
+		var postData = "";
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + "  recieved.");
 		
-		route(handle,pathname);
+		request.setEncoding("utf8");
 
-		response.writeHead(200, {'Content-Type': 'text/plain'});
-		response.write('hello, i know nodejitsu.')
-		response.end();
+		request.addListener("data", function(postDataChunk){
+			postData += postDataChunk;
+			console.log("Received POST data chunk '" + postDataChunk + "'.");
+		});
 
+		request.addListener("end", function() {
+			route(handle, pathname, response, postData);
+		});
 	}
 
 	http.createServer(onRequest).listen(8888);
 	console.log("Server has started on port 8888");
-
 }
 
 exports.start = start;
