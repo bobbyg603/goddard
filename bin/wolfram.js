a21a4b44291a45c78ea0e3305035992b763db624
bobbyg603 <bobbyg603@gmail.com>
2013-11-14 17:58:14 +0000

    cassie's first commit

---
+++ bin/wolfram.js
@@ -0,0 +1,11 @@
+var wolfram = require('wolfram').createClient("2P3U5X-5YLPLVYPJH");
+
+wolfram.query("integrate 2x", function(err, result) {
+  if(err) throw err
+  
+  var newResult = result[0].subpods[0].value;
+  console.log("Result: %j", result);
+  console.log("\n");
+  
+  console.log(newResult);
+})
