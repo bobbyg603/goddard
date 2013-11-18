924b77ff3943b7454e6b0f3b7bb00ddc40f1d400
bobbyg603 <bobbyg603@gmail.com>
2013-11-18 18:49:26 +0000

    Added prompt.js which prompts user and does tts on WolframAlpha results[]

--- bin/sandbox/prompt.js
+++ bin/sandbox/prompt.js
@@ -1,12 +1,49 @@
-  process.stdin.resume();
-  process.stdin.setEncoding('utf8');
-  var util = require('util');
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
 
-  process.stdin.on('data', function (text) {
-    console.log('received data:', util.inspect(text));
-    if (text === 'quit\n') {
-      done();
-    }
+var util = require('util');
+var sys = require('sys');
+var exec = require('child_process').exec;
+var wolfram = require('wolfram').createClient("2P3U5X-5YLPLVYPJH");
+
+process.stdin.on('data', function (text) {
+    
+	//console.log('received data:', util.inspect(text));
+
+	//Use slice() to get rid of the newline character
+	var q = text.slice(0,text.length-1);
+
+	console.log("\nSending Query to Wolfram...");
+	
+	//query wolfram
+	wolfram.query(q, function(err, result){
+		if(err) throw err;
+		
+		var newResult = result[0].subpods[0].value + ", " + result[1].subpods[0].value;
+		//Replace character | with , also need to replace mathematic operations with english equivalents
+		var finalResult = newResult.replace(" |",",").replace("+"," plus ");		
+
+		console.log(result + "\n");
+		console.log(newResult);
+
+	        //might need to consider asynchronous problems
+       		var cmd = "./speech.sh \"" + finalResult + "\"";
+
+		console.log(cmd);
+		
+        	child = exec(cmd, function(error, stdout, stderr) {
+                	//sys.print('stdout:' + stdout);
+                	//sys.print('stderr:' + stderr);
+
+	                if(error !== null) {
+        	                console.log('exec error' + error);
+               		}
+        	});
+		
+	});
+	if (text === 'quit\n') {
+      		done();
+    	}
   });
 
   function done() {
--- bin/sandbox/wolfram.js
+++ bin/sandbox/wolfram.js
@@ -1,9 +1,9 @@
 var wolfram = require('wolfram').createClient("2P3U5X-5YLPLVYPJH");
 
-wolfram.query("integrate 2x", function(err, result) {
-  if(err) throw err
+wolfram.query("8+1", function(err, result) {
+  if(err) throw err;
   
-  var newResult = result[0].subpods[0].value;
+  var newResult = result[0].subpods[0].value +" | "+ result[1].subpods[0].value;
   //console.log("Result: %j", result);
   //console.log("\n");
   
