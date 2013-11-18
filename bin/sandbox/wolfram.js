fb1b194a10caee6c6c39fb331f3e3d9bcb1d45dd
bobbyg603 <bobbyg603@gmail.com>
2013-11-18 17:37:21 +0000

    sandbox cleanup

--- bin/sandbox/wolfram.js
+++ bin/sandbox/wolfram.js
@@ -4,8 +4,8 @@ wolfram.query("integrate 2x", function(err, result) {
   if(err) throw err
   
   var newResult = result[0].subpods[0].value;
-  console.log("Result: %j", result);
-  console.log("\n");
+  //console.log("Result: %j", result);
+  //console.log("\n");
   
   console.log(newResult);
 })
