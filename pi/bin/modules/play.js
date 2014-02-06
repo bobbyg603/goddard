d92e5bbe84d651777898513fb2db697100e9340f
bobbyg603 <bobbyg603@gmail.com>
2014-02-06 13:05:38 -0500

    the directory index.js starts in affects play.js

--- pi/bin/modules/play.js
+++ pi/bin/modules/play.js
@@ -5,7 +5,7 @@ var exec = require("child_process").exec;
 exports.lookup = function lookup(song) {
     
     //var cmd = "ls";
-    var cmd = "cd ../media && mpg123 " + song;  
+    var cmd = "cd ./bin/media && mpg123 " + song;  
 
     exec(cmd,function(error, stdout, stderr) {
 		//Get the directory listing
