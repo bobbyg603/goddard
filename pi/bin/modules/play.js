eea274b79d5a95067030f742c2da2bd834778811
bobbyg603 <bobbyg603@gmail.com>
2014-02-06 17:16:41 +0000

    tweaked play.js for pi, mmmhm piii

--- pi/bin/modules/play.js
+++ pi/bin/modules/play.js
@@ -2,9 +2,9 @@ var exec = require("child_process").exec;
 
 exports.lookup = function lookup(song) {
     
-    //var cmd = "mpg123 " + song;
-    var cmd = "cd ./pi/bin/media && mpg123 " + song;
-    
+    //var cmd = "ls";
+    var cmd = "cd ../media && mpg123 " + song;  
+
     exec(cmd,function(error, stdout, stderr) {
 		//Get the directory listing
 		console.log("stdout: " + stdout);
