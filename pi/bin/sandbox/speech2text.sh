2f898f8b601ee80a82aef2c87026607eb87d5e13
bobbyg603 <bobbyg603@gmail.com>
2014-03-18 17:47:13 -0400

    redid speech2text.sh, cassie added find dogbone 3

--- pi/bin/sandbox/speech2text.sh
+++ pi/bin/sandbox/speech2text.sh
@@ -1,6 +1,7 @@
-echo "Converting Speech to Text..."
-wget -q -U "Mozilla/5.0" --post-file $1 --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v1/recognize?lang=en-us&client=chromium" | cut -d\" -f12  > stt.txt
- 
-echo "You Said:"
-value=`cat stt.txt`
-echo "$value"
+#!/bin/bash
+
+echo "Processing..."
+wget -q -U "Mozilla/5.0" --post-file $1 --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v1/recognize?lang=en-us&client=chromium" | cut -d\" -f12  >stt.txt
+
+echo -n "You Said: "
+cat stt.txt
