40cf4885d08da098e80f1d023ddf3940491318dd
Bobby Galli <bobbyg603@gmail.com>
2014-03-24 22:19:51 -0400

    serial stuff sehn

---
+++ pi/node_arduino.js
@@ -0,0 +1,19 @@
+var SerialPort = require("serialport").SerialPort;
+var serialPort = new SerialPort('/dev/ttyACM0', 
+    {   baudrate: 9600,
+        dataBits: 8,
+        parity: 'none',
+        stopBits: 1,
+        flowControl: false
+    });
+
+serialPort.on("open", function () {
+    console.log('open');
+    serialPort.on('data', function(data) {
+        console.log('data received: ' + data);
+        });
+    serialPort.write("Hello from Raspberry Pi\n", function(err, results) {
+        console.log('err ' + err);
+        console.log('results ' + results);
+        });
+});
