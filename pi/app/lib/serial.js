5c67a5a6463eea22c9d13703634227bfe62bfe9d
Bobby Galli <bobbyg603@gmail.com>
2014-04-01 15:02:55 -0400

    Added serial.js to lib... word up let's hope it werkz

---
+++ pi/app/lib/serial.js
@@ -0,0 +1,19 @@
+var SerialPort = require("serialport").SerialPort;
+var serialPort = new SerialPort('/dev/ttyACM0', 
+    {   baudrate: 9600,
+        dataBits: 8,
+        parity: 'none',
+        stopBits: 1,
+        flowControl: false
+    });
+
+serialPort.on("open", function () {
+    console.log('open');
+    serialPort.on('data', function(data) {
+        console.log('data received: ' + data);
+    });
+    serialPort.write(new Buffer("48", 'ascii'), function(err, results) {
+        console.log('err ' + err);
+        console.log('results ' + results);
+    });
+});
