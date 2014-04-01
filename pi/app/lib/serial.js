4530f9a825dcd941bed7b57cf5a53fc1a6adb65e
Bobby Galli <bobbyg603@gmail.com>
2014-04-01 16:52:39 -0400

    More serial commits

--- pi/app/lib/serial.js
+++ pi/app/lib/serial.js
@@ -1,3 +1,4 @@
+var sendThis = '4';
 var SerialPort = require("serialport").SerialPort;
 var serialPort = new SerialPort("/dev/ttyACM0", {
   baudrate: 9600
@@ -7,9 +8,14 @@ serialPort.on("open", function () {
   console.log('open');
   serialPort.on('data', function(data) {
     console.log('data received: ' + data);
+    if(data==='Y') sendThis = '9';
   });
-  serialPort.write(new Buffer("4",'ascii'), function(err, results) {
+  
+  var write = serialPort.write(new Buffer(sendThis,'ascii'), function(err, results) {
     console.log('err ' + err);
     console.log('results ' + results);
   });
+  
+  for(var i=0;i<100;i++) write();
+
 });
