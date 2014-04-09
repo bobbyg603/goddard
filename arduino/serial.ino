5beb2ed932142c7a3ebd1238d640e18bfbbea34a
Bobby Galli <bobbyg603@gmail.com>
2014-04-09 12:48:46 -0400

    updated arduino code

---
+++ arduino/serial.ino
@@ -0,0 +1,25 @@
+//@bobbyg603
+char inChar;
+
+void setup() {
+  // initialize serial:
+  Serial.begin(9600);
+  pinMode(9,OUTPUT);
+}
+
+void loop() {
+  if(Serial.available()>0){
+    inChar = Serial.read();
+  }
+  
+  if(inChar == '9'){
+    digitalWrite(9,1);
+    delay(500);
+    digitalWrite(9,0);
+    delay(500);
+    digitalWrite(9,1);
+    delay(500);
+    digitalWrite(9,0);
+    inChar='0';
+  }
+}
