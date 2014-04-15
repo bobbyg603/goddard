c89f35621772a698138d3f09569708c775b7d07f
bobbyg603 <bobbyg603@gmail.com>
2014-04-15 23:22:41 +0000

    not quite there

--- pi/app/lib/speak.js
+++ pi/app/lib/speak.js
@@ -3,7 +3,7 @@ var exec = require("child_process").exec;
 
 exports.say = function speak(words) {
     //Determine what program will handle the text to speech 
-    var speak = "espeak -f stt.txt";
+    var speaknow = "espeak -f stt.txt --stdout | aplay";
 
     //Espeak Fix - writes to text file, reads from text, deletes file
     var create = "touch stt.txt && echo '" + words + "' >> stt.txt";
@@ -15,7 +15,7 @@ exports.say = function speak(words) {
     //Execute the linux commands
     exec(create,function(err,stdo,stde){console.log(create);});
 
-    exec(speak,function(error, stdout, stderr) {
+    exec(speaknow,function(error, stdout, stderr) {
 		//sys.print('stdout:' + stdout);
         //sys.print('stderr:' + stderr);
         if(error !== null) {
