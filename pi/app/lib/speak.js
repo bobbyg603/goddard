3e2885ee325421be009a8453814956fbe391577f
bobbyg603 <bobbyg603@gmail.com>
2014-04-15 23:04:33 +0000

    almost fixed speech issuepushing to c9.io to finish cuz it's uber

--- pi/app/lib/speak.js
+++ pi/app/lib/speak.js
@@ -3,16 +3,25 @@ var exec = require("child_process").exec;
 
 exports.say = function speak(words) {
     //Determine what program will handle the text to speech 
-    var cmd = "espeak \"" + words + "\"";
+    var speak = "espeak -f stt.txt";
+
+    //Espeak Fix - writes to text file, reads from text, deletes file
+    var create = "touch stt.txt && echo '" + words + "' >> stt.txt";
+    var del = "rm stt.txt";
 
     //Tell the console goddard is speaking
     console.log("\nespeaking...");
     
-    exec(cmd,function(error, stdout, stderr) {
+    //Execute the linux commands
+    exec(create,function(err,stdo,stde){console.log(create);});
+
+    exec(speak,function(error, stdout, stderr) {
 		//sys.print('stdout:' + stdout);
         //sys.print('stderr:' + stderr);
         if(error !== null) {
             console.log('exec error' + error);
         }
-    });
+	//Delete the leftover stt.txt file after speak is done
+        exec(del,function(err,stdo,stde){console.log(del);});
+   });
 };
