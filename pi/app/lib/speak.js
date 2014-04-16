cf6b22127f602c67c40efb814a4f93d0507bbecf
Bobby Galli <bobbyg603@gmail.com>
2014-04-15 20:00:28 -0400

    switched to festival

--- pi/app/lib/speak.js
+++ pi/app/lib/speak.js
@@ -3,23 +3,9 @@ var exec = require("child_process").exec;
 
 exports.say = function speak(words) {
     //Determine what program will handle the text to speech 
-    var speaknow = "espeak -f stt.txt --stdout | aplay";
+    var saywords = "echo'"+words+"' | festival --tts";
 
-    //Espeak Fix - writes to text file, reads from text, deletes file
-    var create = "touch stt.txt && echo '" + words + "' >> stt.txt";
-    var del = function(){exec("cat \\dev\\null > stt.txt",function(err,stdo,stde){console.log(del);});};
-
-    //Tell the console goddard is speaking
-    console.log("\nespeaking...");
-    
-    //empty the stt file before writing to it
-    //setTimeout(del(),20000);
-    del();
-    
-    //Execute the linux commands
-    exec(create,function(err,stdo,stde){console.log(create);});
-
-    exec(speaknow,function(error, stdout, stderr) {
+    exec(saywords,function(error, stdout, stderr) {
 		//sys.print('stdout:' + stdout);
         //sys.print('stderr:' + stderr);
         if(error !== null) {
