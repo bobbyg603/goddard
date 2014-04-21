80f826368de419fae77a0ec6f12c290ececcfe60
bobbyg603 <bobbyg603@gmail.com>
2014-04-21 15:51:28 +0000

    Misc changes to the pi

--- pi/config/database.js
+++ pi/config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://v2:abc!23$@oceanic.mongohq.com:10069/goddard' // mongodb url configuration
-	
+	//'url' : 'mongodb://v2:abc!23$@oceanic.mongohq.com:10069/goddard' // mongodb url configuration
+	'url' : 'mongodb://goddard:5678b9@ds053638.mongolab.com:53638/goddard'
 }
